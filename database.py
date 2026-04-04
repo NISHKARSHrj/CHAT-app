62ade4c363197d64b4db4c443c2aa29532357e7a
NISHKARSHrj <ronindas00@gmail.com>
2026-04-04 23:02:22 +0530

    first commit

---
+++ database.py
@@ -0,0 +1,28 @@
+import sqlite3
+import datetime
+
+def get_connection():
+    return sqlite3.connect("database.db")
+
+def init_db():
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS messages(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            text TEXT,
+            timestamp TEXT,
+        )
+    """)
+
+    conn.commit()
+    conn.close()
---
+++ routes.py
@@ -0,0 +1,70 @@
+from flask import Flask, app, request, jsonify
+from database import get_connection 
+from datetime import datetime
+def register_routes(app):
+    @app.route('/')
+    def index():
+        return 'Hello, World!'
+    
+    @app.route("/users", methods=["POST"])
+    def create_user():
+        data = request.get_json()
+        name = data.get("name")
+
+        conn = get_connection()
+        # ... (rest of the implementation)  
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            INSERT INTO users (name) VALUES(?)
+
+        """, (name))    
+
+        return {"name": name}
+    
+    @app.route("/send", methods=["POST"])
+    def send_message():
+        data = request.get_json()
+        
+        user_id = data.get("user_id")
+        text = data.get("text")
+
+        datetime = datetime.now().isoformat()
+
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("""
+                INSERT INTO messages (user_id, text,datetime)
+                VALUES(?,?,?)
+                
+        """, (user_id, text, datetime))
+
+        conn.commit()
+        return jsonify({
+            "text": "message sent successfully"
+        })
+    
+    @app.route("/messages", methods=["GET"])
+    def messages():
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            SELECT messages.text, messages.datetime, messages.user_id
+            FROM messages
+            JOIN users ON messages.user_id = user_id
+            ORDER BY messages.id DESC
+        
+        
+        """)
+        rows = cursor.fetchall()
+
+        return jsonify([
+        {
+            "user": r[2],
+            "text": r[0],
+            "timestamp": r[1]
+        }
+        for r in rows
+    ])
